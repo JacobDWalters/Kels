b3c952b1f571af57804d62bcb736f5b85f48b4fb
Jacob <jacob.d.walters22@gmail.com>
2022-12-28 21:37:53 -0500

    listings section complete

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import Image from "next/image";
+import kels from "../public/kels.png";
+import fam from "../public/kokFam.png";
 
 const About = () => {
-  return <div className="container">About</div>;
+  return (
+    <div className="pt-28 lg:pt-44">
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col px-10 align-middle">
+          <h1>Who I Am</h1>
+          <p className="pt-10">
+            Hi friend! It’s nice to meet you! I was born and raised in a town
+            called Caledonia just outside Grand Rapids, MI. I have spent many
+            years enjoying all the beauty that Michigan has to offer. From
+            snowboarding in the winter to boating on Michigan’s Great Lakes in
+            the summer, I am thankful to call this beautiful state home.
+          </p>
+          <p className="pt-4">
+            I have found a joy and passion for real estate. I love helping
+            others find the perfect home! My passion for helping others started
+            with studying medical assisting at Davenport University. After many
+            years of caring for others in this way, I was ready for a change. I
+            am so thankful to be able to care for and help others in a new and
+            exciting way through real estate! It brings me so much joy when I
+            get to hand over the keys to my clients and see the pure happiness
+            on their face.
+          </p>
+        </div>
+
+        <Image
+          src={kels}
+          width="auto"
+          height="auto"
+          alt="kels"
+          className="px-5 rounded md:w-[30%] pt-5"
+        />
+      </div>
+      <div className="flex flex-col md:flex-row-reverse pt-20">
+        <div className="px-10">
+          <h1>My Family</h1>
+          <p className="pt-10">
+            Besides being a realtor, I am also a wife and mama! As our family
+            has grown I have appreciated our home even more. It’s a space for
+            our family and friends to gather, a space of peace in a chaotic
+            world, a place for us to love and grow together. Our homes are such
+            a special place and I hope to help you find this special space to
+            call home! What does home mean to you?
+          </p>
+        </div>
+        <Image
+          src={fam}
+          width="500px"
+          height="auto"
+          alt="fam"
+          className="px-5 md:w-[40%] pt-5"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- components/Listings.jsx
+++ components/Listings.jsx
@@ -1,7 +1,109 @@
 import React from "react";
+import Image from "next/image";
+import houseOne from "../public/houseOne.png";
+import house2 from "../public/house2.png";
+import house3 from "../public/house3.png";
+import house4 from "../public/house4.png";
+import house5 from "../public/house5.png";
 
 const Listings = () => {
-  return <div className="container">Listings</div>;
+  return (
+    <div className="mt-6 bg-black">
+      <h1 className="text-[#ebeaea] flex py-16 px-6 text-center justify-center">
+        Looking For the Perfect Home?
+      </h1>
+      <h3 className="text-[#ebeaea] flex justify-center text-center px-6 text-xl">
+        Check Out Some Featured Listings
+      </h3>
+      <div className="flex justify-center border-b-2 border-[#ebeaea] mx-12 pt-10"></div>
+      <div className="grid sm:grid-cols-3 xl:grid-cols-4 px-4 gap-6 sm:gap-3 mt-10 mx-2">
+        <div className=" relative flex items-center justify-center group">
+          <Image
+            src={houseOne}
+            width="auto"
+            height="auto"
+            alt="house-pic"
+            className="w-[100%] group-hover:opacity-25"
+          />
+          <div className="hidden w-full group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h5 className="flex justify-center text-white">Listed At</h5>
+            <h2 className="flex justify-center text-white pb-2">$320,199</h2>
+            <p className="flex justify-center text-white underline">
+              Click To Check It Out!
+            </p>
+          </div>
+        </div>
+        <div className="relative flex items-center justify-center group">
+          <Image
+            src={house2}
+            width="auto"
+            height="auto"
+            alt="house-pic"
+            className="w-[100%] group-hover:opacity-25"
+          />
+          <div className="hidden w-full group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h5 className="flex justify-center text-white">Listed At</h5>
+            <h2 className="flex justify-center text-white pb-2">$399,999</h2>
+            <p className="flex justify-center text-white underline">
+              Click To Check It Out!
+            </p>
+          </div>
+        </div>
+        <div className="relative flex items-center justify-center group">
+          <Image
+            src={house3}
+            width="auto"
+            height="auto"
+            alt="house-pic"
+            className="w-[100%] group-hover:opacity-25"
+          />
+          <div className="hidden w-full group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h5 className="flex justify-center text-white">Listed At</h5>
+            <h2 className="flex justify-center text-white pb-2">$199,999</h2>
+            <p className="flex justify-center text-white underline">
+              Click To Check It Out!
+            </p>
+          </div>
+        </div>
+        <div className="relative flex items-center justify-center group">
+          <Image
+            src={house4}
+            width="auto"
+            height="auto"
+            alt="house-pic"
+            className="w-[100%] group-hover:opacity-25"
+          />
+          <div className="hidden w-full group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h5 className="flex justify-center text-white">Listed At</h5>
+            <h2 className="flex justify-center text-white pb-2">$599,999</h2>
+            <p className="flex justify-center text-white underline">
+              Click To Check It Out!
+            </p>
+          </div>
+        </div>
+        <div className="relative flex items-center justify-center group">
+          <Image
+            src={house5}
+            width="auto"
+            height="auto"
+            alt="house-pic"
+            className="w-[100%] group-hover:opacity-25"
+          />
+          <div className="hidden w-full group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h5 className="flex justify-center text-white">Listed At</h5>
+            <h2 className="flex justify-center text-white pb-2">$650,000</h2>
+            <p className="flex justify-center text-white underline">
+              Click To Check It Out!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <h3 className="text-[#ebeaea] flex justify-center py-6 text-xl">
+        Contact Me To Explore More!
+      </h3>
+    </div>
+  );
 };
 
 export default Listings;
